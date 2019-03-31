74c846a34d5f8224658319eaf22c9b5581ebe1e9
ozaki <fs65ugumori@yahoo.co.jp>
2019-04-01 08:50:44 +0900

    add strybook chrome screenshot

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,29 @@
 import { addDecorator, configure } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
 import { withInfo } from '@storybook/addon-info';
+import { initScreenshot } from 'storybook-chrome-screenshot';
 
 addDecorator(withKnobs);
 addDecorator(withInfo);
+addDecorator(initScreenshot());
+addDecorator(
+  withScreenshot({
+    viewport: [
+      {
+        width: 320,
+        height: 480,
+        isMobile: true,
+        hasTouch: true,
+      },
+      {
+        width: 414,
+        height: 736,
+        isMobile: true,
+        hasTouch: true,
+      },
+    ],
+  }),
+);
 
 const req = require.context('../src/', true, /stories.(js|tsx)$/);
 
