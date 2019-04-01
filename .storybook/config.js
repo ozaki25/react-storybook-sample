ca311cb7c414dd14da570ddd99d402b2036f9376
ozaki <fs65ugumori@yahoo.co.jp>
2019-04-02 07:40:22 +0900

    remove storybook-chrome-screenshot

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,12 +1,9 @@
 import { addDecorator, configure } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
 import { withInfo } from '@storybook/addon-info';
-import { initScreenshot, withScreenshot } from 'storybook-chrome-screenshot';
 
 addDecorator(withKnobs);
 addDecorator(withInfo);
-addDecorator(initScreenshot());
-addDecorator(withScreenshot());
 
 const req = require.context('../src/', true, /stories.(js|tsx)$/);
 
